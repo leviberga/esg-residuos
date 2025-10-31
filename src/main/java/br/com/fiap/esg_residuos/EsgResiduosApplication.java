0363c7f2a6aee4ad39c57e6b9e82c32da007b1c9
Levi Bergamascki <lbergamascki@gmail.com>
2025-10-31 15:47:34 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/esg_residuos/EsgResiduosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.esg_residuos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgResiduosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgResiduosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/esg_residuos/EsgResiduosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.esg_residuos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgResiduosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
